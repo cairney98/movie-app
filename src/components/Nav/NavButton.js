6f80f785ff2edd73d5e1bd46d50368acb7e93f7a
cairney98 <mcairney92@gmail.com>
2021-11-09 18:57:22 +0000

    Altered nav test5

--- src/components/Nav/NavButton.js
+++ src/components/Nav/NavButton.js
@@ -6,7 +6,7 @@ const NavButton = ({ colour, tag, path, paddingx }) => {
       className={`w-64 px-${paddingx} rounded hover:bg-${colour}-500 mx-0.5 py-2 opacity-70 tracking-wider bg-gray-900 text-gray-400 hover:text-gray-100 hover:opacity-100 transition-all hover:py-3 duration-300`}
       href={`${path}`}
     >
-      { colour}{paddingx}
+      {tag}
     </a>
   );
 };
