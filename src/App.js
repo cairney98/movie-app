99e73c2ecb20e187912dc1d84b21004b96e0235d
cairney98 <mcairney92@gmail.com>
2021-11-10 10:09:28 +0000

    Added person detail page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Nav from "./components/Nav/NavBar";
 import Movie from "./components/Movie";
+import Person from "./components/Person";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:movieId" element={<Movie />} />
+        <Route path="/people/:personId" element={<Person />} />
       </Routes>
     </main>
   );
